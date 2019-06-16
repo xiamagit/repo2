192e7f28cb23460558c55489f09c0dfe1a5a61ad
xiaoma <xiaoma@0620.com>
2019-06-16 19:48:30 +0800

    test代码提交

---
+++ test/src/main/java/com/itheima/test/Test.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好github");
+        System.out.println("你好github");
+        System.out.println("你好github");
+        System.out.println("你好github");
+        System.out.println("你好github");
+        System.out.println("你好github");
+    }
+}
